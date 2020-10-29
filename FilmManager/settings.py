698e29e7bf574375f52e9b42723535615fa5eee5
vladislin <vlad.slinchuk@gmail.com>
2020-10-29 14:49:35 +0200

    v2

--- FilmManager/settings.py
+++ FilmManager/settings.py
@@ -1,3 +1,5 @@
+import django_heroku
+
 """
 Django settings for FilmManager project.
 
@@ -126,3 +128,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+django_heroku.settings(locals())
