e09f8cab6543d39e1ec346d7ea2df786533219fc
vladislin <vlad.slinchuk@gmail.com>
2020-10-30 14:42:55 +0200

    first-2

--- FilmManager/settings.py
+++ FilmManager/settings.py
@@ -80,12 +80,26 @@ WSGI_APPLICATION = 'FilmManager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'film-api',
+        'USER': 'postgres',
+        'PASSWORD': 'pipodu65',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
